5bfda192d003379ebda4d727c4d327bd4cdd01ca
Rulft44 <dimucciorubengermania@gmail.com>
2024-05-26 02:13:46 +0100

    :art: Blahaj, idk (Blahajh?)

--- src/main/java/net/rulft/blades_and_relics/BladesAndRelics.java
+++ src/main/java/net/rulft/blades_and_relics/BladesAndRelics.java
@@ -1,35 +1,25 @@
 package net.rulft.blades_and_relics;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.rulft.blades_and_relics.item.ModItems;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
-
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BladesAndRelics.MOD_ID)
 public class BladesAndRelics {
 
     public static final String MOD_ID = "blades_and_relics";
 
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public BladesAndRelics() {
-        // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
@@ -41,15 +31,13 @@ public class BladesAndRelics {
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // Some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        LOGGER.info("smh my balls - illuc 2006-2023");
     }
 }
