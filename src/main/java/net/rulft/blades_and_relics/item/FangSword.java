0607ca6c36bdc19e6dc84c3f6d4f36a75b911ad1
Rulft44 <dimucciorubengermania@gmail.com>
2024-05-26 02:12:40 +0100

    :art: Fixed Dragon Dagger

---
+++ src/main/java/net/rulft/blades_and_relics/item/FangSword.java
@@ -0,0 +1,38 @@
+package net.rulft.blades_and_relics.item;
+
+import com.github.alexthe666.iceandfire.entity.EntityFireDragon;
+import com.github.alexthe666.iceandfire.entity.EntityIceDragon;
+import com.github.alexthe666.iceandfire.entity.EntityLightningDragon;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class FangSword extends SwordItem {
+    public FangSword(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add((new TranslatableComponent("fang_sword.hurt")).withStyle(ChatFormatting.GREEN));
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
+        if(pTarget instanceof EntityFireDragon || pTarget instanceof EntityIceDragon || pTarget instanceof EntityLightningDragon){
+            pTarget.hurt(DamageSource.playerAttack((Player) pAttacker), 9.5f);
+        }
+        return super.hurtEnemy(pStack, pTarget, pAttacker);
+    }
+}
--- src/main/java/net/rulft/blades_and_relics/item/ModItems.java
+++ src/main/java/net/rulft/blades_and_relics/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.rulft.blades_and_relics.item;
 
 import com.github.alexthe666.iceandfire.IceAndFire;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,15 +8,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.rulft.blades_and_relics.BladesAndRelics;
 
+import static com.github.alexthe666.iceandfire.item.IafItemRegistry.DRAGONBONE_TOOL_MATERIAL;
+
+
 public class ModItems {
+
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, BladesAndRelics.MOD_ID);
 
 
-    public static final RegistryObject<Item> DRAGON_FANG = ITEMS.register("dragon_fang",
-            () -> new Item(new Item.Properties().tab(IceAndFire.TAB_ITEMS).stacksTo(1)));
-    public static final RegistryObject<Item> FANG_SWORD = ITEMS.register("fang_sword",
-            () -> new Item(new Item.Properties().tab(IceAndFire.TAB_ITEMS).stacksTo(1)));
+    public static final RegistryObject<Item> FANG_SWORD = ITEMS.register("fang_sword", () -> new FangSword(DRAGONBONE_TOOL_MATERIAL, 3, -2.4F, (new Item.Properties()).tab(IceAndFire.TAB_ITEMS)));
+    public static final RegistryObject<Item> DRAGON_FANG = ITEMS.register("dragon_fang", () -> new Item(new Item.Properties().tab(IceAndFire.TAB_ITEMS).stacksTo(1)));
 
 
     public static void register(IEventBus eventBus) {
